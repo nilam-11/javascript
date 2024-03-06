d5943028ccda72508986ee66014bac84bce4088a
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-03-06 22:59:12 +0530

    json

---
+++ basics/objects_2.js
@@ -0,0 +1,18 @@
+//de structure
+
+const course = {
+    coursename: "learn js",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+const {courseInstructor: instructure} = course
+console.log(instructor); //Hitesh
+
+// {
+//     "name":"Nilam",
+//     "course": "js",
+//     "price": "free"
+// }
+
+
